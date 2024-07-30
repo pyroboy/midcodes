4fc7a7e67030cd8ac1595841598a8c110e4d787f
midcodes <arjomagno@gmail.com>
2024-07-30 16:41:26 +0800

    rat

--- svelte.config.js
+++ svelte.config.js
@@ -6,11 +6,8 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      // Use 'edge' for edge runtime, or 'nodejs18.x' for Node.js runtime
-      runtime: 'nodejs18.x',
-
-      // an array of dependencies that esbuild should not bundle
-      // 'external': [],
+      // Remove the runtime specification
+      // Vercel will automatically choose the appropriate runtime
 
       // if true, will split your app into multiple functions
       // instead of creating a single one for the entire app
