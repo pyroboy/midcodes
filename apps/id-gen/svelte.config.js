e70cbfeec05fd51a75fc2cad8e7b4e1f67075db7
midcodes <arjomagno@gmail.com>
2025-12-13 00:05:39 +0800

    a

--- apps/id-gen/svelte.config.js
+++ apps/id-gen/svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
